f28d63c1e26efa58c2072f85346ab5ed630a5fdd
dmknapp2385 <dmknapp2385@gmail.com>
2022-06-20 12:25:42 -0700

    update product quantity in cart functionality

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,7 +1,35 @@
 import { existedOperationTypeMessage } from "graphql/validation/rules/UniqueOperationTypes";
 import React from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
 const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
+
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -15,9 +43,11 @@ const CartItem = ({ item }) => {
         <input
           type="number"
           placeholder="1"
+          onChange={onChange}
           value={item.purchaseQuantity}
+
         ></input>
-        <span role="img" aria-label="trash">
+        <span role="img" aria-label="trash" onClick={removeFromCart}>
           🗑️
         </span>
       </div>
